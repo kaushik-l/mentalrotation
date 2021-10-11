5005867633624e360f5eea3879d9b574783b585b
Kaushik Lakshminarasimhan <jkl9@nyu.edu>
2021-10-11 11:50:20 -0400

    standalone rnn

--- rand3d_rnn.py
+++ rand3d_rnn.py
@@ -17,70 +17,78 @@ import matplotlib.pyplot as plt
 
 npr.seed(1)
 N = 200 #RNN units
-S = 10 #input, not used here
+S = 24 #input (2 x 12)
 R = 1 #readout
 B = 1 #batches per epoch
-NK = 10 # number of conditions
+NK = 5 # number of conditions
 kmax = 20 # max distance between input patterns
-tmin, tmax = 10, 30 # max reaction time
+tmin, tmax = 1, 10 # max reaction time
 
-Nepochs = 10000
+Nepochs = 40000
 doplot = True
-Nepochsplot = Nepochs / 5 #plot output every certain number of epochs
+Nepochsplot = Nepochs / 40 #plot output every certain number of epochs
 dt = .1
-T = 40
-period = 10
+T = 15
+period = 5
 NT = int(T/dt)
 input_scale = 0.1
 dist_scale = 1
 
-lr = .0001
+lr = .001
 sig = 0 #noise in initial state
 
 t = dt*np.arange(NT)
 
-dist = Variable(torch.arange(1, NK + 1, 1),requires_grad=False) * (dist_scale/NK)
-#tstar = Variable(torch.arange(tmin, tmax, (tmax - tmin) / NK),requires_grad=False) # linear model
-tstar = (10*torch.sin(2*np.pi*dist) + 20).round()  # sin model
-s1 = input_scale * (2 * Variable(torch.rand(int(S/2), B),requires_grad=False).repeat(NT, 1, 1) - 1)   # reference pattern
-ds = input_scale * (2 * Variable(torch.rand(int(S/2), B),requires_grad=False).repeat(NT, 1, 1) - 1)
-s2 = Variable(torch.empty(NK, NT, int(S/2), B),requires_grad=False)
-rtarg = Variable(torch.empty(NK, NT, R, B),requires_grad=False)
+dist = Variable(torch.arange(1, NK + 1, 1), requires_grad=False) * (dist_scale/NK)
+tstar = Variable(torch.arange(tmin, tmax + 1, (tmax + 1 - tmin) / NK), requires_grad=False) # linear model
+#tstar = (10*torch.sin(2*np.pi*dist) + 20).round()  # sin model
+
+# ***** this is for pre-training; load data from aec here instead of s1 *****
+
+s1 = input_scale * (2 * Variable(torch.rand(int(S/2), B), requires_grad=False).repeat(NT, 1, 1) - 1)   # reference pattern
+#ds = input_scale * (2 * Variable(torch.rand(int(S/2), B), requires_grad=False).repeat(NT, 1, 1) - 1)
+#s2 = Variable(torch.empty(NK, NT, int(S/2), B), requires_grad=False)
+rtarg = Variable(torch.empty(NK, NT, R, B), requires_grad=False)
 for i in range(NK):
-  s2[i] = s1 + dist[i] * ds                                                   # pattern to match
-  rtarg[i] = Variable(torch.zeros(NT,R,B),requires_grad=False)
-  rtarg[i, int(tstar[i]/dt):, 0, 0] = 0.1
+  #s2[i] = s1 + dist[i] * ds                                                     # pattern to match
+  rtarg[i] = Variable(torch.zeros(NT, R, B), requires_grad=False)
+  rtarg[i, :int(tstar[i] / dt), 0, 0] = torch.arange(0, 1, dt / tstar[i])       # ramp
+  rtarg[i, int(tstar[i] / dt):, 0, 0] = 1                                         # bound
 
-ws0 = np.random.standard_normal([N,S]).astype(np.float32)/np.sqrt(S)
-J0 = np.random.standard_normal([N,N]).astype(np.float32)/np.sqrt(N)
-wr0 = np.random.standard_normal([R,N]).astype(np.float32)/np.sqrt(N)
-b0 = np.zeros([N,1]).astype(np.float32)
+ws0 = np.random.standard_normal([N, S]).astype(np.float32)/np.sqrt(S)
+J0 = np.random.standard_normal([N, N]).astype(np.float32)/np.sqrt(N)
+wr0 = np.random.standard_normal([R, N]).astype(np.float32)/np.sqrt(N)
+b0 = np.zeros([N, 1]).astype(np.float32)
 
-ws = Variable(torch.from_numpy(ws0),requires_grad=True)
-J = Variable(torch.from_numpy(J0),requires_grad=True)
-wr = Variable(torch.from_numpy(wr0),requires_grad=True)
-b = Variable(torch.from_numpy(b0),requires_grad=True)
+ws = Variable(torch.from_numpy(ws0), requires_grad=True)
+J = Variable(torch.from_numpy(J0), requires_grad=True)
+wr = Variable(torch.from_numpy(wr0), requires_grad=True)
+b = Variable(torch.from_numpy(b0), requires_grad=True)
 
-opt = torch.optim.Adam([J,wr,b,ws],lr=lr)
+opt = torch.optim.Adam([J, wr, b, ws], lr=lr)
 
-xinit = 0.1*torch.randn(N,1)
-losslist2 = []
+xinit = 0.01*torch.randn(N, 1)
+losslist, losslist2 = [], []
 prevt = time.time()
 for ei in range(Nepochs):
-    print(ei,"\r",end='')
-    x = xinit + sig*torch.randn(N,B)
-    xa = torch.zeros(NT,N,B) #save the hidden states for each time bin for plotting
-    r = torch.zeros(NT,R,B)
+
+    print(ei, "\r", end='')
+    x = xinit + sig*torch.randn(N, B)
+    xa = torch.zeros(NT, N, B) #save the hidden states for each time bin for plotting
+    r = torch.zeros(NT, R, B)
     idx = npr.randint(0, NK, size=None)
-    s = torch.cat((s1, s2[idx]), dim=1)
+    ds = input_scale * (2 * Variable(torch.rand(int(S / 2), B), requires_grad=False).repeat(NT, 1, 1) - 1)
+    s = torch.cat((s1, s1 + dist[idx] * ds), dim=1)
+    #s = torch.cat((s1, s2[idx]), dim=1)
 
     for ti in range(NT):
-        x = x + dt*(-x + torch.tanh(ws.mm(s[ti,:,:]) + J.mm(x) + b))
-        xa[ti,:,:] = x
-        r[ti,:,:] = wr.mm(x)
+        x = x + dt*(-x + torch.tanh(ws.mm(s[ti, :, :]) + J.mm(x) + b))
+        xa[ti, :, :] = x
+        r[ti, :, :] = wr.mm(x)
 
-    loss = torch.sum(torch.pow(r - rtarg[idx],2)) / (NT*B)
-    losslist2.append(loss.item())
+    tbound = int(tstar[idx] / dt)
+    loss = torch.sum(torch.pow(r[:tbound] - rtarg[idx, :tbound], 2)) / (tbound*B)
+    losslist.append(loss.item())
     print('\r' + str(ei + 1) + '/' + str(Nepochs) + '\t Err:' + str(loss.item()), end='')
 
     #do BPTT
@@ -88,92 +96,29 @@ for ei in range(Nepochs):
     opt.step()
     opt.zero_grad()
 
-    if doplot and (ei == 0 or ei%Nepochsplot == 0 or ei%(Nepochs - 1) == 0):
-        plt.clf()
+    if doplot and (ei == 0 or ei % Nepochsplot == 0 or ei > (Nepochs - 20)):
+        if ei < (Nepochs - 20):
+            plt.clf()
         #plt.subplot(211)
-        plt.plot(rtarg.numpy()[idx, :,0,0])
-        plt.plot(r.detach().numpy()[:,0,0])
+        plt.plot(rtarg.numpy()[idx, :, 0, 0])
+        plt.plot(r.detach().numpy()[:, 0, 0])
         #plt.subplot(212)
         #plt.plot(xa.detach().numpy()[:,:,0])
         plt.pause(0.001) #I had to do this to get the plot to refresh, not sure why
         plt.draw()
-print("train time: ",time.time() - prevt)
+print("train time: ", time.time() - prevt)
 
 from scipy.signal import medfilt
 p = plt.figure(figsize=(6, 6))
 plt.loglog(losslist, color='red')
-plt.loglog(losslist2, color='blue')
+#plt.loglog(losslist2, color='blue')
 ax = plt.gca()
 plt.xticks(fontsize=16)
 plt.yticks(fontsize=16)
 plt.xlabel('Training Epoch', fontsize=18)
 plt.ylabel('Loss', fontsize=18)
 plt.tight_layout()
-p.savefig('rnnloss.png', dpi=200)
-
-### test rnn
-import seaborn as sns
-Nepochs = 50
-distances = []
-responsetime = []
-for ei in range(Nepochs):
-    print(ei,"\r",end='')
-    x = xinit + sig*torch.randn(N,B)
-    xa = torch.zeros(NT,N,B) #save the hidden states for each time bin for plotting
-    r = torch.zeros(NT,R,B)
-    idx = npr.randint(0, NK, size=None)
-    
-    dist = npr.rand()
-    s2 = s1 + dist * ds + (.02 * npr.rand())
-    s = torch.cat((s1, s2), dim=1)
-
-    for ti in range(NT):
-        x = x + dt*(-x + torch.tanh(ws.mm(s[ti,:,:]) + J.mm(x) + b))
-        xa[ti,:,:] = x
-        r[ti,:,:] = wr.mm(x)
-
-    distances.append(dist)
-    if not len(np.argwhere(r[:, 0, 0].detach().numpy() > 0.08)) == 0:
-      responsetime.append(np.argwhere(r[:, 0, 0].detach().numpy() > 0.08)[0] / (10*tmax))
-    else:
-      responsetime.append(0)
-    #responsetime.append(np.argmax(r[:, 0, 0].detach().numpy()) / (10*tmax))
-
-p = plt.figure(figsize = (6,6))
-sns.regplot(distances, responsetime, marker='o', scatter_kws={'s':20}, color='red')
-ax = plt.gca()
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.xticks(fontsize=16)
-plt.yticks(fontsize=16)
-plt.xlabel('Distance between latents', fontsize=18)
-plt.ylabel('Reaction time', fontsize=18)
-plt.tight_layout()
-p.savefig('rnncorr.png', dpi=200)
-
-npr.seed(1)
-N = 200 #RNN units
-S = 10 #input, not used here
-R = 1 #readout
-B = 1 #batches per epoch
-NK = 10 # number of conditions
-kmax = 20 # max distance between input patterns
-tmin, tmax = 10, 30 # max reaction time
-
-Nepochs = 10000
-doplot = True
-Nepochsplot = Nepochs / 5 #plot output every certain number of epochs
-dt = .1
-T = 40
-period = 10
-NT = int(T/dt)
-input_scale = 0.1
-dist_scale = 1
-
-lr = .0001
-sig = 0 #noise in initial state
-
-t = dt*np.arange(NT)
+#p.savefig('rnnloss.png', dpi=200)
 
 dist = Variable(torch.arange(1, NK + 1, 1),requires_grad=False) * (dist_scale/NK)
 tstar = Variable(torch.arange(tmin, tmax, (tmax - tmin) / NK),requires_grad=False) # linear model
